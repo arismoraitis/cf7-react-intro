e3a4d7e47fb7fb1127529f5dcc69e15849791d6c
arismoraitis <aris.moraiths@gmail.com>
2025-11-09 02:41:23 +0200

    feat: add scope logo

---
+++ src/components/ScopeLogo.tsx
@@ -0,0 +1,17 @@
+const ScopeLogo = () => {
+
+    return (
+        <>
+
+        <img className="my-4 h-16"
+            src="/scope-logo.svg"
+            alt="Scope Logo"
+        />
+        </>
+
+    )
+
+
+}
+
+export default ScopeLogo;
